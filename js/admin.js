04f0f6dcced072560130f3e504bb7e1d774b119a
refitrihidayatullah <refitrihidayatullah19@gmail.com>
2022-11-07 18:51:40 +0700

    add tampilan dashboard admin

---
+++ js/admin.js
@@ -0,0 +1,10 @@
+// navbar = document.querySelector(".sidebar-menu").querySelectorAll("a");
+
+// navbar.forEach(element => {
+//     element.addEventListener("click", function() {
+//         navbar.forEach(nav => nav.classList.remove("active"));
+
+//         this.classList.add("active");
+//     })
+
+// });
